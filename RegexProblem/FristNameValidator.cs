c0a826acccd047c975374a7187bdcb3d250ced45
“rohyapawar2912” <rohitpawar8530@gmail.com>
2023-07-24 10:37:59 +0530

    [Rohit]Add: // add all files

---
+++ RegexProblem/FristNameValidator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace RegexProblem
+{
+    public class FristNameValidator
+    {
+        private const string FirstNamePattern = "^[A-Z][a-zA-Z]{2,}$";
+
+        public static bool IsValidFirstName(string firstName)
+        {
+            return Regex.IsMatch(firstName, FirstNamePattern);
+        }
+
+    }
+    
+}
---
+++ RegexProblem/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your first name:");
+            string firstName = Console.ReadLine();
+
+            if (FristNameValidator.IsValidFirstName(firstName))
+            {
+                Console.WriteLine("Valid first name entered!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid first name. It should start with a capital letter and have a minimum of 3 characters.");
+            }
+
+        }
+    }
+}
